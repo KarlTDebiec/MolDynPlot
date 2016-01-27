75b6e4ce0c3fd02f4a98977d476a33e03953b4e0
Karl Debiec <karl.t.debiec@gmail.com>
2016-01-27 17:41:07 -0500

    much faster and more memory-efficient

--- cpptraj2hdf5.py
+++ cpptraj2hdf5.py
@@ -43,23 +43,27 @@ if __name__ == "__main__":
       help     = "HDF5 file to which to dataset will be output")
 
     # Parse arguments
-    kwargs      = vars(parser.parse_args())
-    infile      = kwargs["infile"]
-    outfile     = kwargs["outfile"]
+    kwargs  = vars(parser.parse_args())
+    infile  = kwargs["infile"]
+    outfile = kwargs["outfile"]
     if kwargs["kind"] == "dihedral":
-        field_dtype = "f4"
-        address     = "dihedral"
+        dtype = np.float32
+        scaleoffset = 4
+        address = "dihedral"
     elif kwargs["kind"] == "hbond":
-        field_dtype = "i1"
-        address     = "hbond"
+        dtype = np.uint8
+        scaleoffset = 1
+        address = "hbond"
     elif kwargs["kind"] == "perresrmsd":
-        field_dtype = "f4"
-        address     = "perresrmsd"
+        dtype = np.float32
+        scaleoffset = 4
+        address = "perresrmsd"
     elif kwargs["kind"] == "secstruct":
-        field_dtype = "i1"
-        address     = "secstruct"
+        dtype = np.float32
+        scaleoffset = 3
+        address = "secstruct"
 
-    # Load input file
+    # Determine dimensions of dataset
     with open(devnull, "w") as fnull:
 
         # Determine field names and prepare dtype
@@ -69,51 +73,22 @@ if __name__ == "__main__":
         if fields.pop(0) != "#Frame":
             raise TypeError("cpptraj2hdf5 currently only supports input in " +
                             "the form of '#Frame field_1 field_2 ...'")
-        dtype = [(str("Frame"), np.int64)]
-        for field in fields:
-            dtype += [(str(field), field_dtype)]
+        n_fields = len(fields)
 
     # Open hdf5 file
     with h5py.File(outfile) as hdf5_file:
+        # Instantiate resizable dataset
 
-        # Just use loadtxt for now
-        data = np.loadtxt(infile, dtype=dtype)
+        def iter_func():
+            with open(infile, 'r') as open_file:
+                next(open_file)
+                for line in open_file:
+                    line = line.split()[1:]
+                    for item in line:
+                        yield dtype(item)
 
-        hdf5_file.create_dataset(address, data=data,
-          chunks=True, compression="gzip", maxshape=(None,))
-
-
-    # May need to use code below for large files that cannot fit in memory
-    # Very slow; there is probably a much better solution
-#            # Instantiate resizable dataset
-#            hdf5_file.create_dataset(address, data=np.empty(1, dtype),
-#              chunks=True, compression="gzip", maxshape=(None,))
-#
-#            first_line = 2
-#            first_index = 0
-#            while True:
-#                last_line = first_line + chunk_size - 1
-#
-#                # Load frame indexes for chunk
-#                command = "sed -n '{0},{1}p' {2} ".format(first_line, last_line, infile) + \
-#                          "| cut -c1-8 | tr -d '\n'"
-#                process = Popen(command, stdout=PIPE, stderr=fnull, shell=True)
-#                input_bytes = bytearray(process.stdout.read())
-#                frame = np.array(np.frombuffer(input_bytes, dtype="S8", count=int((len(input_bytes)) / 8)), np.int64)
-#                last_index = first_index + frame.size
-#
-#                # Load data for chunk
-#                command = "sed -n '{0},{1}p' {2} | cut -c9- | tr -d '\n'".format(first_line, last_line, infile)
-#                process = Popen(command, stdout=PIPE, stderr=fnull, shell=True)
-#                input_bytes = bytearray(process.stdout.read())
-#                data = np.array(np.frombuffer(input_bytes, dtype="S13", count=int((len(input_bytes)) / 13)), np.int8)
-#
-#                # Store data in hdf5 file
-#                hdf5_file[address].resize(size=(last_index,))
-#                hdf5_file[address][first_index:last_index] = np.rec.array(frame, dtype=[dtype[0]])
-#                hdf5_file[address][first_index:last_index] = np.rec.array(data,  dtype=dtype[1:])
-#
-#                first_line  += chunk_size
-#                first_index += chunk_size
-#                if frame.size < chunk_size:
-#                    break
+        data = np.fromiter(iter_func(), dtype=dtype)
+        data = data.reshape((-1, n_fields))
+        print(data, data.shape)
+        hdf5_file.create_dataset(address, data=data, dtype=dtype,
+          chunks=True, compression="gzip", scaleoffset=scaleoffset)
