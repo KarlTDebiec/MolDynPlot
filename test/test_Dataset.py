36f53d39d09a241ef3935d7f4505f47ec7ef626d
Karl Debiec <karl.t.debiec@gmail.com>
2016-07-14 22:20:52 -0600

    testing travis

--- test/test_Dataset.py
+++ test/test_Dataset.py
@@ -8,10 +8,12 @@
 #   This software may be modified and distributed under the terms of the
 #   BSD license. See the LICENSE file for details.
 def test_TimeSeriesDataset():
-    from pandas.util.testing import assert_frame_equal
     from moldynplot.Dataset import TimeSeriesDataset
+
+    # Read text
     text_df = TimeSeriesDataset(infile="test/data/p53/rmsd.dat").timeseries_df
-    hdf5_df = TimeSeriesDataset(infile="test/data/p53/rmsd.h5").timeseries_df
     assert hash(text_df.to_string()) == 8096571869640597227
+
+    # Read hdf5
+    hdf5_df = TimeSeriesDataset(infile="test/data/p53/rmsd.h5").timeseries_df
     assert hash(hdf5_df.to_string()) == 8096571869640597227
-    assert_frame_equal(text_df, hdf5_df)
