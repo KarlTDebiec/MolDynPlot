50303450680c308aba50f02f01177c3413c6186a
Karl Debiec <karl.t.debiec@gmail.com>
2016-07-15 07:35:11 -0600

    testing packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name             = "MolDynPlot",
+    version          = "0.1",
+    packages         = ["moldynplot",
+                        "moldynplot.myplotspec",
+                        "moldynplot.fpblockaverager"],
+    license          = "BSD 3-clause",
+    long_description = open("README.rst").read()
+)
