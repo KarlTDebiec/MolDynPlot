aadc08218a8482f4dca97b52b0e571f9c28244f6
Karl Debiec <karl.t.debiec@gmail.com>
2016-07-15 19:30:54 -0600

    travis & coverage working

--- moldynplot/test/test_Dataset.py
+++ moldynplot/test/test_Dataset.py
@@ -7,10 +7,11 @@
 #
 #   This software may be modified and distributed under the terms of the
 #   BSD license. See the LICENSE file for details.
-def test_TimeSeriesDataset():
-    from pandas.util.testing import assert_frame_equal
-    from moldynplot.Dataset import TimeSeriesDataset
-
+################################### MODULES ###################################
+from pandas.util.testing import assert_frame_equal
+from moldynplot.Dataset import TimeSeriesDataset
+################################## FUNCTIONS ##################################
+def test_rmsd():
     # Read cpptraj
     cpptraj_df = TimeSeriesDataset(
       infile="data/p53/rmsd.cpptraj",
@@ -28,8 +29,40 @@ def test_TimeSeriesDataset():
     # Compare
     assert_frame_equal(cpptraj_df, hdf5_df)
     assert_frame_equal(cpptraj_df, text_df)
-    assert_frame_equal(text_df, hdf5_df)
 
     # Write text
 
     # Write hdf5
+
+def test_rmsd_perres():
+    # Read cpptraj
+    cpptraj_df = TimeSeriesDataset(
+      infile="data/p53/rmsd_perres.cpptraj",
+      dt=0.1,
+      toffset=-0.1).timeseries_df
+
+    # Read text
+    text_df = TimeSeriesDataset(
+      infile="data/p53/rmsd_perres.dat").timeseries_df
+
+    # Read hdf5
+    hdf5_df = TimeSeriesDataset(
+      infile="data/p53/rmsd_perres.h5").timeseries_df
+
+    # Read legacy hdf5
+    lgcy_df = TimeSeriesDataset(
+      infile="data/p53/rmsd_perres_legacy.h5",
+      dt=0.1).timeseries_df
+
+    # Compare
+    assert_frame_equal(cpptraj_df, hdf5_df)
+    assert_frame_equal(cpptraj_df, text_df)
+    assert_frame_equal(cpptraj_df, lgcy_df)
+
+    # Write text
+
+    # Write hdf5
+
+if __name__ == "__main__":
+    test_rmsd()
+    test_rmsd_perres()
